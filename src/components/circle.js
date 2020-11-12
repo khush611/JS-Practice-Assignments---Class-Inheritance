74fdfaaf4483e06dfabcbc1b4cebe6c779c1365c
Khusboo Kumari <36759104+khush611@users.noreply.github.com>
2020-11-12 14:36:16 +0530

    changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,13 @@
 // import using require
-
+const Shape = require("./shape.js");
 // declare class
-
-
+class Circle extends Shape {
+  constructor() {
+    super();
+  }
+  calculateArea() {
+    return "calculateArea of child";
+  }
+}
 // export class using module.exports
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,14 @@
 // declare class
-
+class Shape {
+  constructor() {
+    this.color = "#eee45d";
+  }
+  drawShape() {
+    return "drawShape";
+  }
+  calculateArea() {
+    return "calculateArea";
+  }
+}
 // export class using module.exports
+module.exports = Shape;
